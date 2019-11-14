26ac37b9d9738452f0aa747f5e698ec4e39a22b6
unknown <tbmoore85@gmail.com>
2019-11-14 10:19:10 -0500

    Submitting project

---
+++ index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    
+  } else {
+    x.style.display = "flex";
+    
+  }
+  
+}
